290f9e1f6445d789ec3f9a1212c61eba49edecc2
Amar Khaliq <khal178@icloud.com>
2021-07-18 00:26:34 +0100

    fixed errors

--- js/ratings.js
+++ js/ratings.js
@@ -6,19 +6,18 @@ function collect_ratings() {
     };
     let rating = 0;
     const elements = document.querySelectorAll(".rating");
-    elements.forEach((element) => {
+    elements.forEach(element => {
         rating = parseInt(element.id.replace('star', ''));
-        count += parseInt(element.value);
-        sum += parseInt(element.value) * rating;
+        ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value) * rating;
     });
-    if(count != 0) {
-        average = sum / count;
+    if(ratings.count !== 0) {
+        ratings.average = ratings.sum / ratings.count;
     }
     return ratings;
 }
 
 document.addEventListener('change', (event) => {
     const ratings = collect_ratings();
-    const average = document.querySelector("#average");
-    average.value = ratings.average.toFixed(2);
+    document.querySelector("#average").value = ratings.average.toFixed(2);
 });
